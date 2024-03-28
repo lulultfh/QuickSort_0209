288b08347e14e2de6711b857c07d0c1d198737e6
lulultfh <punyalulu05@gmail.com>
2024-03-28 14:00:07 +0700

    add comments for variable global

--- 209_quicksort/209_quicksort.cpp
+++ 209_quicksort/209_quicksort.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 //array of integers to hold values
 int arr[20];
-int cmp_count = 0;
-int mov_count = 0;
+int cmp_count = 0; //number of comparasion
+int mov_count = 0; //number of data movements
 int n;
 
 int main()
