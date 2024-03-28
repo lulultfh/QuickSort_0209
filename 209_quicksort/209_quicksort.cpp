739b22470fd07a241816a0216c35f27cee9ef390
lulultfh <punyalulu05@gmail.com>
2024-03-28 15:15:49 +0700

    add prosedur display

--- 209_quicksort/209_quicksort.cpp
+++ 209_quicksort/209_quicksort.cpp
@@ -89,6 +89,20 @@ void q_short(int low, int high)
     q_short(j + 1, high); //step 13
 }
 
+void display() {
+    cout << "\n-----------------------------" << endl;
+    cout << "Sorted Array" << endl;
+    cout << "-------------------------------" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+
+    cout << "\n\nNumber of comparasion: " << cmp_count << endl;
+    cout << "Number of data movements: " << mov_count << endl;
+}
+
 int main()
 {
     std::cout << "Hello World!\n";
